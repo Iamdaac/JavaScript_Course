a1bd3b2895e44f816234478f2b4aa625b9f5e4d9
Iamdaac <diego.alvarado.1801@gmail.com>
2025-09-22 21:18:33 -0500

    Pig game firts part

--- Modules/06-Modal/starter/script.js
+++ Modules/06-Modal/starter/script.js
@@ -3,7 +3,7 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('.show-modal'); //Seleccionar todas las clases que sean iguales
+const btnsOpenModal = document.querySelectorAll('.show-modal'); //(All) Selecciona todas las clases que sean iguales
 
 const openModal = function () {
   //Funcion DRY para abrir el modal
@@ -23,4 +23,11 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
   btnCloseModal.addEventListener('click', closeModal);
   overlay.addEventListener('click', closeModal);
+
+  //Close modal with key = 'Escape' / 'ESC'
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  });
 }
